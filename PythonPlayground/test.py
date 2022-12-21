0b6f33a061fc3cb4261d432cb0a209a62f9ca3e5
BunbunTheVile <evolvingbunny@gmail.com>
2022-12-21 18:51:02 +0100

    .

--- PythonPlayground/test.py
+++ PythonPlayground/test.py
@@ -1,2 +1,21 @@
-print("Hi")
-input()
+def isPrime(number: int) -> int:
+    if number == 0 or number == 1:
+        return False
+    if number == 2:
+        return True
+    
+    i: int = 2
+    while i <= number / 2:
+        if number % i == 0:
+            return False
+        i += 1
+
+    return True
+
+for i in range(200):
+    if isPrime(i):
+        print(f"{i} yes")
+    else:
+        print(f"{i} no")
+
+
