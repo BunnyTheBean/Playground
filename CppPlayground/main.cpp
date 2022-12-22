7556090db4e8fc909b9d778e22499655539d0e44
Bunny <evolvingbunny@gmail.com>
2022-12-22 14:45:21 +0100

    .

--- CppPlayground/main.cpp
+++ CppPlayground/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
-    for (int i = 0; i < 10; i++)
-    {
-        cout << i;
+void berechne(int n) {
+    cout << "Teiler von " << n << " sind:\n";
+    for (int teiler = 1; teiler <= n; ++teiler) {
+        if (n % teiler == 0) {
+            cout << teiler << ", ";
+        }
     }
+    cout << endl;
+}
+
+int main(int argc, const char* argv[]) {
+    int wert = 0;
+    if (argc <= 1) {
+        cout << "Geben Sie eine Zahl ein: ";
+        cin >> wert;
+        if (!cin) {
+            return 1;
+        }
+    } else {
+        wert = stoi(argv[1]);
+    }
+    berechne(wert);
+    return 0;
 }
