b2576dda102e77bd2ba58ff771be2597e142508d
BunbunTheVile <evolvingbunny@gmail.com>
2022-12-30 01:01:26 +0100

    a

--- PythonPlayground/test.py
+++ PythonPlayground/test.py
@@ -1,21 +1,2 @@
-def isPrime(number: int) -> int:
-    if number == 0 or number == 1:
-        return False
-    if number == 2:
-        return True
-    
-    i: int = 2
-    while i <= number / 2:
-        if number % i == 0:
-            return False
-        i += 1
-
-    return True
-
-for i in range(200):
-    if isPrime(i):
-        print(f"{i} yes")
-    else:
-        print(f"{i} no")
-
-
+x: int = "abc"
+print(x)
