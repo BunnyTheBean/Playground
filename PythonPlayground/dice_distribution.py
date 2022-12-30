75bbcb34f547f3028d2ca1f41cd838bff4417603
BunbunTheVile <evolvingbunny@gmail.com>
2022-12-30 14:54:21 +0100

    a

---
+++ PythonPlayground/dice_distribution.py
@@ -0,0 +1,57 @@
+class DiceDistribution:
+    number: int
+    sides: int
+    possibilities: int
+    #modifier: int TODO
+
+    def __init__(self, number_of_dice: int, sides_of_dice: int):
+        self.number = number_of_dice
+        self.sides = sides_of_dice
+        self.possibilities = self.sides ** self.number
+    
+    def binomial_coefficient(n: int, k: int) -> int:
+        k = (n - k) if (n - k) < k else k
+        total = 1
+        i = 0
+        while i < k:
+            total = total * (n - i) / (k - i)
+            i += 1
+        return int(round(total))
+
+    def permutations_for_result(self, result: int) -> int:
+        total = 0
+        imax = (result - self.number) / self.sides
+        i = 0
+
+        while i <= imax:
+            sign = 1 if i % 2 == 0 else -1
+            factor1 = DiceDistribution.binomial_coefficient(self.number, i)
+            factor2 = DiceDistribution.binomial_coefficient(result - self.sides * i - 1, result - self.sides * i - self.number)
+            total += sign * factor1 * factor2
+            i += 1
+        
+        return int(round(total))
+    
+    def probability_of_result(self, result: int) -> float:
+        return self.permutations_for_result(result) / self.possibilities
+
+    def probability_of_range(self, lower: int, upper: int) -> float:
+        if lower > upper:
+            lower, upper = upper, lower
+
+        probability: float = 0.0
+        for i in range (lower, upper+1):
+            probability += self.probability_of_result(i)
+        return probability
+
+def main():
+    dist = DiceDistribution(6, 6)
+    
+    prob = dist.probability_of_range(15, 20)
+    print(f"15-20:\t{round(prob * 100, 2)}%")
+
+    prob = dist.probability_of_range(20, 15)
+    print(f"20-15:\t{round(prob * 100, 2)}%")
+
+if __name__ == "__main__":
+    main()
